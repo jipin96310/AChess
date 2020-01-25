c4289dc51e2ab66f5bc0a572bb40ed25972f7d1f
zhaohengSun <18667345163@163.com>
2020-01-25 17:22:03 +0800

    封装透明棋盘

--- AChess/ViewController.swift
+++ AChess/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, SCNPhysicsContactDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -24,10 +24,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+        //sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +35,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        configuration.planeDetection = .horizontal
+        
+        //tap gesture added
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(onTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        tapGestureRecognizer.cancelsTouchesInView = false
+        // We want to receive the frames from the video
+        sceneView.session.delegate = self
+        // Run the view's session
+        sceneView.session.run(configuration)
+        //set contact delegate
+        sceneView.scene.physicsWorld.contactDelegate = self
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -72,4 +83,64 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    @objc func onTap(sender: UITapGestureRecognizer) {
+            guard let sceneView = sender.view as? ARSCNView else {return}
+            let touchLocation = sender.location(in: sceneView)
+            let hitTestResult = sceneView.hitTest(touchLocation, types: [.existingPlaneUsingExtent])
+            if !hitTestResult.isEmpty {
+                self.initPlayerBoard(hitTestResult: hitTestResult.first!)
+            }
+        }
+    func initPlayerBoard(hitTestResult: ARHitTestResult) {
+        let playerBoardNode = createPlayerBoard()
+        //playerBoardNode.eulerAngles = SCNVector3(45.degreesToRadius, 0, 0)
+        //playGroundNode.geometry?.firstMaterial?.isDoubleSided = true
+        //playGroundNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        let positionOFPlane = hitTestResult.worldTransform.columns.3
+        let xP = positionOFPlane.x
+        let yP = positionOFPlane.y
+        let zP = positionOFPlane.z
+        playerBoardNode.position = SCNVector3(xP,yP + 0.01,zP)
+        playerBoardNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        //playGroundNode.physicsBody?.categoryBitMask = BitMaskCategoty.playGround.rawValue
+        //playGroundNode.physicsBody?.contactTestBitMask = BitMaskCategoty.baseCard.rawValue
+        self.sceneView.scene.rootNode.addChildNode(playerBoardNode)
+    }
+    
+    // MARK: - ARSCNViewDelegate
+
+    public func renderer(_: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let _ = anchor as? ARPlaneAnchor else { return nil }
+
+        // We return a special type of SCNNode for ARPlaneAnchors
+       return customPlaneNode()
+    }
+
+    public func renderer(_: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor,
+            let planeNode = node as? customPlaneNode else {
+            return
+        }
+        planeNode.update(from: planeAnchor)
+    }
+
+    public func renderer(_: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor,
+            let planeNode = node as? customPlaneNode else {
+            return
+        }
+        planeNode.update(from: planeAnchor)
+    }
+}
+extension Int {
+    var degreesToRadius: Double { return Double(self) * .pi/180}
+}
+//func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+//    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+//}
+func -(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x - right.x, left.y - right.y, left.z - right.z)
+}
+func averageVector(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + (right.x - left.x) / 2, right.y, right.z)
 }
